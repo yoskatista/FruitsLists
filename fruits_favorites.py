232b1193de51c1446a04f3671e59496dd3adc3b8
Mikael Cassamassimo Borges <66427859+yoskatista@users.noreply.github.com>
2021-02-18 13:30:03 -0300

    FruitsFavorites

---
+++ fruits_favorites.py
@@ -0,0 +1,10 @@
+fruits_favorites = ['Banana', 'Maça','Mexerica']
+
+if 'Banana' in fruits_favorites:
+    print('\nBananananananana delicious')
+
+if 'Maça' in fruits_favorites:
+    print('Maça na listinha hmmm deliciusss nhanhanhanahnaha')
+
+if 'Mexerica' in fruits_favorites:
+    print('Mexericas também são uma deliviucfnjdfnee')
